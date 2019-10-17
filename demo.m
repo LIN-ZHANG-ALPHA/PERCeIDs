f03fb426c214aa6d901c7db75c7d1ebc60879929
LIN-ZHANG-ALPHA <linzhang0529@gmail.com>
2019-10-16 21:39:39 -0400

    Update demo.m

--- demo.m
+++ demo.m
@@ -1,43 +1,29 @@
 
 
-
-
-
-
-
-
 clc; clear all;
-
 addpath(genpath(pwd))
 rng('default')
 %%  load data
-
 data_path  = 'data/MIT/';
-data_name = 'mit_hour_all';
+data_name  = 'mit_hour_all';
 load([data_path,'/',data_name,'.mat']) % 
 
 data_GT_name = 'communities_lab'; % _o: overlap commu; _no: non-overlap commu
 load([data_path,'/',data_GT_name,'.mat']) % 
 
 data_save = ['demo_',data_name];
-
-opts.GT = communities_lab;
+opts.GT   = communities_lab;
 opts.K    = size(opts.GT,2); %  num of communities
 
-%%
-
+%%  parameter setting
 opts.lambda_0           = 1;
 opts.lambda_1           = 1;
 opts.lambda_2           = 1;
 
-opts_pcd.max_iter           = 200;
-opts_pcd.Pmax               = 10;
+opts_pcd.max_iter       = 200;
+opts_pcd.Pmax           = 10;
 
-
-
-
-
-%%
+%%  main function 
 
 [Fac.Fac_PCD_L1] = detect_community_PCD(X,opts,opts_pcd);
 
